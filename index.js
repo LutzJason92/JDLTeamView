38614a572c00a73bcbba845c80fd95578a495d31
Jason <LutzJason92@gmail.com>
2021-12-02 19:43:48 -0700

    js updated
    .thens added for questions. Array created as add staff. can answer yes or no to make another employee

---
+++ index.js
@@ -0,0 +1,117 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/engineer.js");
+const Manager = require("./lib/manager.js");
+const Intern = require("./lib/intern.js");
+const renderTemplateLiteral = require("./src/src.js");
+
+let staff = [];
+// prompts user input
+const entry = [
+  {
+    type: "input",
+    message: "What is employee First and Last name?",
+    name: "NAME",
+  },
+  {
+    type: "input",
+    message: "Enter Employee ID#:",
+    name: "GETID",
+  },
+  {
+    type: "input",
+    message: "Enter Employee email address:",
+    name: "GETEMAIL",
+  },
+  {
+    type: "list",
+    message: "Enter Employee :",
+    name: "GETROLE",
+    choices: ["Intern", "Engineer", "Manager"],
+  },
+];
+
+function init(entry) {
+  inquirer.prompt(entry).then((response) =>
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the Managers office number",
+          name: "OFFICE",
+          when: function (_) {
+            return response.GETROLE === "Manager";
+          },
+        },
+        {
+          type: "input",
+          message: "What is the Engineers Github Username?",
+          name: "GITHUB",
+          when: function (_) {
+            return response.GETROLE === "Engineer";
+          },
+        },
+        {
+          type: "input",
+          message: "At what school does the Intern Study?",
+          name: "SCHOOL",
+          when: function (_) {
+            return response.GETROLE === "Intern";
+          },
+        },
+      ])
+
+      .then(({ OFFICE, GITHUB, SCHOOL }) => {
+        console.log(OFFICE, GITHUB, SCHOOL, response);
+
+        if (OFFICE) {
+          let newManager = new Manager(
+            response.NAME,
+            response.GETID,
+            response.GETEMAIL,
+            OFFICE
+          );
+          staff.push(newManager);
+        }
+        if (GITHUB) {
+          let newEngineer = new Engineer(
+            response.NAME,
+            response.GETID,
+            response.GETEMAIL,
+            GITHUB
+          );
+          staff.push(newEngineer);
+        }
+        if (SCHOOL) {
+          let newIntern = new Intern(
+            response.NAME,
+            response.GETID,
+            response.GETEMAIL,
+            SCHOOL
+          );
+          staff.push(newIntern);
+        }
+        inquirer
+          .prompt({
+            type: "list",
+            message: "Would you link to add another staff member?",
+            name: "ADDSTAFF",
+            choices: ["Yes", "No"],
+          })
+          .then(({ ADDSTAFF }) => {
+            if (ADDSTAFF === "Yes") {
+              init(entry);
+            } else {
+              fs.writeFile(
+                "index.html",
+                renderTemplateLiteral(staff),
+                (err) => {
+                  err ? console.log(err) : console.log("htmlfile is ready!");
+                }
+              );
+            }
+          });
+      })
+  );
+}
+init(entry);
---
+++ src/src.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+// create a wire frame HTML
+function renderTemplateLiteral(staff) {
+  console.log(staff);
+  return ``;
+}
+
+// template literal that runs "for each" that loops over each user response to create new card
+//const employeeCard = ``;
+
+module.exports = renderTemplateLiteral;
